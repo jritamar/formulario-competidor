8b3f7ee0663f5c544720d4c524140fee31a02d6a
Itamar Junior <104800630+jritamar@users.noreply.github.com>
2022-05-30 17:50:16 -0300

    Add files via upload
    V3.0

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+include "servicos/servicoMensagemSessao.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -5,84 +11,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario Campeonato</title>
-    <style>
-        body{
-            background-color: gray;
-        }
-       
-       .box1{
-        position: absolute;
-        color: black;
-        left: 23%;
-        top: 15%;
-        transform: translate(-50/-50);
-        background-color: rgba(0, 0, 0, 0.6);
-        padding: 1px 10px 10px;
-        border-radius: 10px;
-        width: 50%;
-       }
-       fieldset{
-        border: 3px solid black;
-        border-radius: 20px;
-        padding: 10px;
-       }
-       legend{
-           border: 3px solid black;
-           position: relative;
-           text-align: center;
-           padding: 10px;
-           border-radius: 10px;
-           color: black;
-
-       }
-       .divb{
-           position: relative;
-           text-align: center;
-              
-      }
-      .labelInput{
-          position: absolute;
-          pointer-events: none;
-          top: 0;
-          left: 0;
-          
-      }
-      .inputUser{
-        background: none;
-        border: none;
-        border-bottom: 1px solid white;
-        outline: none;
-        color: white;
-        font-size: 20px;
-        width: 100%;
-        letter-spacing: 2px;
-      }
-      .inputUser:focus ~ .labelInput,
-      .inputUser:valid ~ .labelInput{
-        top: -20px;
-        font-size: 12px;
-        color: black;
-        }
-      #submit{
-        background-image: linear-gradient(to right,rgb(211, 211, 211), rgb(0, 0, 0));
-        width: 60%;
-        border: none;
-        padding: 10px;
-        color: white;
-        font-size: 15px;
-        cursor: pointer;
-        border-radius: 10px;
-        text-align: center;
-        }
-       #submit:hover{
-        background-image: linear-gradient(to right,rgb(0, 0, 0), rgb(211, 211, 211));
-        }
-       
-       
-    </style>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <form action="script.php" method="post">
+    
+        <?php
+        $mensagemDeErro = obterMensagemErro();
+        if(!empty($mensagemDeErro)){
+            echo "<script type='text/javascript'>alert('$mensagemDeErro');</script>";
+        }   
+        $mensagemDeSucesso = obterMensagemSucesso();
+        if(!empty($mensagemDeSucesso)){
+            echo "<script type='text/javascript'>alert('$mensagemDeSucesso');</script>";
+        }    
+        ?>     
          <div class="box1">
              <br>
     <fieldset>
@@ -91,13 +34,13 @@
                 <legend><b>FORMULARIOS PARA COMPETIDORES</b></legend>
     <br><br>
     <div class="divb">    
-    <input type="text" name="nome" class="inputUser" required>  
+    <input type="text" name="nome" class="inputUser">  
     <label for="nome" class="labelInput"><b>Nome:</b></label>  
     </div>
     <br>
     <br>
     <div class="divb">    
-    <input type="idade" name="idade" class="inputUser" required>
+    <input type="idade" name="idade" class="inputUser">
     <label for="idade" class="labelInput"><b>Idade:</b></label>
     </div>
     <br><br>
--- script.php
+++ script.php
@@ -1,49 +1,18 @@
 <?php
 
-$categorias = [];
-$categorias[] = 'Infantil';
-$categorias[] = 'Adolescente';
-$categorias[] = 'Adulto';
-
-// print_r($categorias)
+session_start();
+include "servicos/servicoMensagemSessao.php";
+include "servicos/servicoValidacao.php";
+include "servicos/servicoCategoriaCompetidor.php";
 
 @$nome = $_POST['nome']; 
 @$idade = $_POST['idade'];
 
-// var_dump($nome);
-// var_dump($idade);
-// return 0;
+defineCategoriaCompetidor($nome,$idade);
 
-if(empty($nome)){
-    echo "O nome não pode ser vazio";
-    return;
-}
-if(empty($idade)){
-    echo "A idade não pode ser vazio";
-    return;
-}
-if(strlen($nome) < 3){
-    echo "O nome não pode ter menos que 3 caracteres";
-    return;
-}
+header ("location: index.php");
 
-if(strlen($nome) > 30){
-    echo "O nome não pode ter mais que 30 caracteres";
-    return;
-}
-if(!is_numeric($idade)){
-    echo "Campo só aceita números como caracteres!";
-    return;
-}
-
-if($idade >= 6 and $idade <= 12 ){
-   echo "a href='windows.open" . "<b>" . "O competidor " . $nome . " faz parte da categoria " .  $categorias[0] . "!";
-}
-elseif($idade >= 13 and $idade <= 18){
-    echo "<b>" . "O competidor " . $nome . " faz parte da categoria " .  $categorias[1] . "!";
-}
-else{
-    echo "<b>" . "O competidor " . $nome . " faz parte da categoria " .  $categorias[2] . "!";
-}
 
 ?>
+
+   
